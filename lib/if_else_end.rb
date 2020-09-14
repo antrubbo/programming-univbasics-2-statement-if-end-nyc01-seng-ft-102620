0e994019665e94c4c57297077d7c8c57bd674300
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-14 21:22:20 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,8 +2,4 @@
 # current_time = Time.now
 # current_time = current_time.to_i
 
-if Time.now.to_i % 2 == 0 
-  puts "Even!"
-else
-  puts "Odd!"
-end
+Time.now.to_i % 2 == 0 ? ( puts "Even!") : (puts "Odd!")
